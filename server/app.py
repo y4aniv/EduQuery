7ce1cea0da3c3bf8bcef11e70bcc1538cfc9f9cf
Yaniv Douieb <yanivdouieb@icloud.com>
2024-03-26 22:37:21 +0100

    server: Flask instance configurations

--- server/app.py
+++ server/app.py
@@ -5,3 +5,9 @@ import pandas  # `pandas` permet de manipuler des données
 import flask as fl  # `flask` permet de créer une API REST
 from flask_cors import CORS as fl_cors  # `flask_cors` permet de gérer les CORS
 from flask_compress import Compress as fl_compress  # `flask_compress` permet de compresser les réponses
+
+# Configurations de l'instance de Flask
+app = fl.Flask(__name__)  # Créer une instance de l'application Flask
+fl_cors(app, resources={r"/api/*": {"origins": "*"}})  # Activer les CORS pour l'API REST
+fl_compress(app)  # Activer la compression des réponses
+
