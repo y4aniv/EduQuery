86f947a672d3913227796c01753f895849a35198
Yaniv Douieb <yanivdouieb@icloud.com>
2024-03-26 23:41:06 +0100

    server: add IO module

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ import pandas  # `pandas` permet de manipuler des données
 import flask as fl  # `flask` permet de créer une API REST
 from flask_cors import CORS as fl_cors  # `flask_cors` permet de gérer les CORS
 from flask_compress import Compress as fl_compress  # `flask_compress` permet de compresser les réponses
+import io  # `io` permet de gérer les entrées/sorties de données
 
 # Configurations de l'instance de Flask
 app = fl.Flask(__name__)  # Créer une instance de l'application Flask
